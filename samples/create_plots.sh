1d78cd313f676160a4cfadb35f434baf184ebe45
Ray Sun <xiaoquqi@gmail.com>
2021-05-15 23:34:22 +0800

    Add daemon and rename mount dir

--- samples/create_plots.sh
+++ samples/create_plots.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 docker run -it \
+  -d \
   --name chia-docker \
   -v ~/.chia:/root/.chia \
-  -v /chia_tmp:/plots-tmp \
-  -v /chia_data:/plots-final \
   -v ~/.local:/root/.local \
+  -v /plots-tmp:/plots-tmp \
+  -v /plots-final:/plots-final \
   chia-docker \
   chia plots create -t /plots-tmp -d /plots-final -b 2048
